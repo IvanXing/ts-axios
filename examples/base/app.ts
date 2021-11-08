16655f5e4fc49b22c985868a0133fda2e81260b9
xingyifan <xingyifan10@qq.com>
2021-11-08 13:58:49 +0800

    处理url的get请求参数

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -2,73 +2,64 @@ import axios from '../../src/index'
 
 axios({
   method: 'get',
-  url: '/simple/get',
+  url: '/base/get',
   params: {
-    a: 1,
-    b: 2
+    foo: ['bar', 'baz']    // /base/get?foo[]=bar&foo[]=baz
   }
 })
 
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: ['bar', 'baz']
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: {
-//       bar: 'baz'
-//     }
-//   }
-// })
-//
-// const date = new Date()
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     date
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: '@:$, '
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: 'bar',
-//     baz: null
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get#hash',
-//   params: {
-//     foo: 'bar'
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get?foo=bar',
-//   params: {
-//     bar: 'baz'
-//   }
-// })
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'   // /base/get?foo=%7B%22bar%22:%22baz%22%7D
+    }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: 'bar',
+    baz: null
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'bar'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+  params: {
+    bar: 'baz'
+  }
+})
 //
 // axios({
 //   method: 'post',
