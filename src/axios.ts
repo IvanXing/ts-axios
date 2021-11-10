48b1767d054ae6aa21564069f7b89375110f3b3d
xingyifan <xingyifan10@qq.com>
2021-11-10 21:24:19 +0800

    [接口扩展]混合调用

--- src/axios.ts
+++ src/axios.ts
@@ -1,44 +1,19 @@
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import { AxiosInstance } from './types'
+import Axios from './core/Axios'
+import { extend } from './helpers/util'
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res) // 处理res的data
-  })
+// 把原型属性和实例属性 全部拷贝到instance上
+function createInstance(): AxiosInstance {
+  const context = new Axios()
+
+  const instance = Axios.prototype.request.bind(context)
+  // instance 是一个函数， 目的是 axios({}) 可以直接这样调用 request，不用写request
+
+  extend(instance, context)
+
+  return instance as AxiosInstance
 }
 
-// 处理xhr的参数
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  config.headers = transformHeaders(config) // 在data处理之前先处理header，data有stringfy转化
-  config.data = transformRequestData(config)
-}
-
-// 处理请求body
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-// 处理url
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-// 处理headers
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-// 处理响应data
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+const axios = createInstance()
 
 export default axios
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -7,3 +7,12 @@ export function isDate(val: any): val is Date {
 export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
+
+// 把from拷贝到to中 返回结果是 T&U 类型 交叉类型
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    // to[key] = from[key] 加了断言
+    ;(to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
+}
